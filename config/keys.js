eddcbf86ede9de5ab39327e214e13a1765d1c68c
Eddie Xu <eddiexu82@gmail.com>
2021-01-16 12:18:14 -0500

    Finished Redux to delete/add items to list

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-    mongoURI: 'mongodb+srv://eddie:eddie123@cluster0.x105b.mongodb.net/test'
-}
+    mongoURI: process.env.mongoURI
+} 
